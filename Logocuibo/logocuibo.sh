fd1288471625cf6737e0f4457c9285d278e2d261
rA9 <38068746+bariscukur@users.noreply.github.com>
2020-02-23 23:29:33 +0300

    added cleanup

--- Logocuibo/logocuibo.sh
+++ Logocuibo/logocuibo.sh
@@ -1,5 +1,5 @@
 ./ibootim logo.png logo.v
-img4tool -c logo.v2 -t logo -d ibo logo.v
-img4tool -c logo.final -p logo.v2 -s ticket.shsh2
+./img4tool -c logo.v2 -t logo -d ibo logo.v
+./img4tool -c logo.final -p logo.v2 -s ticket.shsh2
 rm -rf logo.v2
 rm -rf logo.v 
